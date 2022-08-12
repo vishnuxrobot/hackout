36ebee2887c29078fbcdec758d71c3becbc7fff0
vishnuxrobot <36655133+vishnuxrobot@users.noreply.github.com>
2022-08-12 14:24:23 +0530

    Add files via upload

---
+++ js/conn.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDrH9Tc1R4GhnFEvS6U-NjrxrQ_QSbpUhk",
+  authDomain: "hack-out.firebaseapp.com",
+  databaseURL: "https://hack-out.firebaseio.com",
+  projectId: "hack-out",
+  storageBucket: "hack-out.appspot.com",
+  messagingSenderId: "283104078860",
+  appId: "1:283104078860:web:654ce68d451fed16059229",
+  measurementId: "G-38WZGQ6Y3Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // firebase.analytics();
---
+++ js/getmacid.js
@@ -0,0 +1,3 @@
+const getmac = require(['getmac']).default
+
+alert(getmac())
